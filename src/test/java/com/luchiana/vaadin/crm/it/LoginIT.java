b9a964f0599b706b16a25a7f94b7d28b98ce3372
irina-pachina <tudupm@gmail.com>
2023-02-01 20:09:59 +0100

    add e2e tests using Vaadin TestBench

---
+++ src/test/java/com/luchiana/vaadin/crm/it/LoginIT.java
@@ -0,0 +1,23 @@
+package com.luchiana.vaadin.crm.it;
+
+import com.luchiana.vaadin.crm.it.pageObjects.LoginViewElement;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginIT extends AbstractTest {
+    public LoginIT() {
+        super("");
+    }
+
+    @Test
+    public void loginAsValidUserSucceeds() {
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertTrue(loginView.login("user", "userpass"));
+    }
+
+    @Test
+    public void loginAsInvalidUserFails() {
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertFalse(loginView.login("user", "invalid"));
+    }
+}
