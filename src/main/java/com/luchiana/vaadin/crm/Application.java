b78f01d11068d6347999bc8b146f597dee3d8b83
irina-pachina <tudupm@gmail.com>
2023-02-01 17:40:14 +0100

    enable PWA and offline placeholder

--- src/main/java/com/luchiana/vaadin/crm/Application.java
+++ src/main/java/com/luchiana/vaadin/crm/Application.java
@@ -2,15 +2,22 @@ package com.luchiana.vaadin.crm;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
+@PWA(
+        name = "Vaadin CRM",
+        shortName = "VaadinCRM",
+        offlinePath="offline.html",
+        offlineResources = { "./images/offline.png"}
+)
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
